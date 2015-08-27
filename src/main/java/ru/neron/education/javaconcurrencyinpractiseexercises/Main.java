b63389a904f0d0d9cbc4bc45cc66442d80397a40
neron <neron@yandex-team.ru>
2015-08-27 12:47:35 +0300

    test 2

--- src/main/java/ru/neron/education/javaconcurrencyinpractiseexercises/Main.java
+++ src/main/java/ru/neron/education/javaconcurrencyinpractiseexercises/Main.java
@@ -7,14 +7,48 @@ import java.util.concurrent.*;
  */
 public class Main {
 
+    int x = 0;
+    int y = 0;
+
+    public Main(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public static void main(String... args) {
-        Mutable mutable = new Mutable();
+        Main point = new Main(0, 0);
 
-        for (int i = 0; i < 100; i++) {
-            new Thread(() -> mutable.a = 1).start();
-            new Thread(() -> mutable.a = 2).start();
+        new Thread(() -> {
+            for (int i = 0; i < 20; i++) {
+                int x = point.getX();
+                int y = point.getY();
+                if (x != y) {
+                    System.err.printf("O_o %d %d \n", x, y);
+                }
+            }
+        }).start();
+
+        for (int i = 0; i < 2_000; i++) {
+            for (int j = 0; j < 100; j++) {
+                point.setX(j);
+                point.setY(j);
+            }
         }
-
-        System.out.println(mutable.a);
     }
 }
--- src/main/java/ru/neron/education/javaconcurrencyinpractiseexercises/kalishenko/LockOne.java
+++ src/main/java/ru/neron/education/javaconcurrencyinpractiseexercises/kalishenko/LockOne.java
@@ -1,7 +1,32 @@
 package ru.neron.education.javaconcurrencyinpractiseexercises.kalishenko;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
 /**
  * Created by neron on 09.08.15.
  */
+// assume 2 only thread
+// алгоритм Петерсона
 public class LockOne {
+    private volatile boolean[] flag = new boolean[2];
+    private volatile int victim;
+
+    public void lock() {
+        int threadId = 1; // get thread id
+//        int threadId = 0; // get thread id
+
+        flag[threadId] = true; // want
+        victim = threadId;  // уступаем другому потоку
+        int anotherThreadId = 1 - threadId;
+        while (flag[anotherThreadId] && victim == threadId) {}
+    }
+
+    public void unlock() {
+        int threadId = 1; // get thread id
+//        int threadId = 0; // get thread id
+
+        flag[threadId] = false;
+    }
 }
